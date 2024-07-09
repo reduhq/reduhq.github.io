5c79908bfda6dca1db5dbfe46226afec29241ff4
Rey Halsall <reduhq25@gmail.com>
2024-07-08 23:38:11 -0600

    Adding SVG Icons to the Icons path

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,22 @@ import icon from "astro-icon";
 export default defineConfig({
   site:'https://reduhq.github.io',
   base: '/portfolio',
-  integrations: [tailwind(), icon()]
+  integrations: [
+    tailwind(), 
+    icon({
+      svgoOptions:{
+        plugins:[
+          // {
+          //   name: 'removeStyleElement' // Elimina elementos <style> dentro del SVG
+          // },
+          {
+            name: 'removeAttrs', // Elimina atributos específicos
+            params: {
+              attrs: ['color'] // Elimina los atributos color, fill, y stroke
+            }
+          }
+        ]
+      }
+    })
+  ]
 });
