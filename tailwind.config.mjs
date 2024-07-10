1c871915bea10c189b6ffdb67be6d24b9e2183d9
Rey Halsall <reduhq25@gmail.com>
2024-07-10 17:24:16 -0600

    Fixing bugs

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,6 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	safelist:[
+		"bg-[#d63aff66]",
+		"bg-[#2ca5e044]",
+		"bg-[#222]",
+		"bg-[#193449]",
+		"bg-[#006e6366]",
+		"bg-[#31619299]",
+		"bg-[#000]",
+		"bg-[#235a9799]",
+		"bg-[#23272f]"
+	],
 	theme: {
 		extend: {
 			colors:{
