d95999ecfcc0d363c0004e7f65ca5d1a863efc08
Rey Halsall <reduhq25@gmail.com>
2024-07-18 18:30:58 -0600

    Adding the email sending feature

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,35 +1,33 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import icon from "astro-icon";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  site:'https://reduhq.github.io',
+  site: 'https://reduhq.github.io',
   base: '/portfolio',
-  i18n:{
-    defaultLocale:'es',
-    locales:['es', 'en'],
-    routing:{
+  i18n: {
+    defaultLocale: 'es',
+    locales: ['es', 'en'],
+    routing: {
       prefixDefaultLocale: false
     }
   },
-  integrations: [
-    tailwind(), 
-    icon({
-      svgoOptions:{
-        plugins:[
-          // {
-          //   name: 'removeStyleElement' // Elimina elementos <style> dentro del SVG
-          // },
-          {
-            name: 'removeAttrs', // Elimina atributos específicos
-            params: {
-              attrs: ['color'] // Elimina los atributos color, fill, y stroke
-            }
-          }
-        ]
-      }
-    })
-  ]
+  integrations: [tailwind(), icon({
+    svgoOptions: {
+      plugins: [
+      // {
+      //   name: 'removeStyleElement' // Elimina elementos <style> dentro del SVG
+      // },
+      {
+        name: 'removeAttrs',
+        // Elimina atributos específicos
+        params: {
+          attrs: ['color'] // Elimina los atributos color, fill, y stroke
+        }
+      }]
+    }
+  }), react()]
 });
